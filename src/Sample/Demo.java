8fac2f4950dd77f2c19151d1c0fb87cf05232cf2
RAKESH REDDY <123718737+rakeshr2727@users.noreply.github.com>
2023-04-27 18:03:21 +0530

    Rakesh[#3rd change in project]

---
+++ src/Sample/Demo.java
@@ -0,0 +1,13 @@
+package Sample;
+
+public class Demo {
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hello varsha");
+		System.out.println("Hello Rutuja");
+		
+	}
+
+}
